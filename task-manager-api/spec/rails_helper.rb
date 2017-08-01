470f06f0c4e60cdf875701e2c46c7fae9eb69ea1
Kevin Allen <kevinallenlf@gmail.com>
2017-08-01 02:04:23 +0000

    added tests for delete, update, some beautiful refactors

--- task-manager-api/spec/rails_helper.rb
+++ task-manager-api/spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -69,6 +69,7 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
--- task-manager-api/spec/requests/api/v1/users_spec.rb
+++ task-manager-api/spec/requests/api/v1/users_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
+    let!(:user) { create(:user) }
+    let(:user_id) { user.id }
+    let(:headers) do
+      {
+         "Accept" => "application/vnd.taskmanager.v1",
+         'Content-type' => 'application/json'
+      }
+    end
 
-
-    before { host! 'api.taskmanager.dev'}
+    before { host! 'api.taskmanager.dev' }
 
     describe 'GET users/:id' do
-      let!(:user) { create(:user) }
-      let(:user_id) { user.id }
-
+      
       before do
-          headers = { 'Accept' => 'application/vdn.taskmanager.v1' }
-          get "/users/#{user_id}", params: {}, headers: headers
+        
+        get "/users/#{user_id}", params: {}, headers: headers
       end
 
       context 'when user exists' do
         it 'returns the user' do
-            user_response = JSON.parse(response.body, symbolize_names: true)
-            expect(user_response[:id]).to eq(user_id)
+            expect(json_body[:id]).to eq(user_id)
         end  
 
         it 'return status 200' do
@@ -36,39 +40,79 @@ RSpec.describe 'Users API', type: :request do
 
     end
 
-    describe 'POST /users' do
-    
-      before do
-		    headers = { 'Accept' => 'application/vdn.taskmanager.v1' }
-    	  post '/users', params: { user: user_params }, headers: headers
+    describe "POST /users" do
+        before do
+          post '/users', params: { user: user_params }.to_json, headers: headers
+        end
+
+        context 'when the request params are valid' do
+          let(:user_params) { attributes_for(:user) }
+
+          it 'returns status code 201' do
+            expect(response).to have_http_status(201)
+          end
+
+          it 'returns the json data for the created user' do
+            expect(json_body[:email]).to eq(user_params[:email])
+          end
+        end
+
+        context 'when the request params are invalid' do
+          let(:user_params) { attributes_for(:user, email: 'invalid.email@') }
+
+          it 'returns status code 422' do
+            expect(response).to have_http_status(422)
+          end
+
+          it 'returns the json data for the errors' do
+            expect(json_body).to have_key(:errors)
+          end
+        end
       end
 
-			context 'when the request params are valid' do
-			  let(:user_params) { attributes_for(:user) }
 
-				it 'returns status code 201' do
-          expect(response).to have_http_status(201)
-        end
+    describe 'PUT /users' do 
 
-        it 'should return created user' do
-          user_response = JSON.parse(response.body, symbolize_names: true)
-          expect(user_params[:email]).to eq(user_response[:email])
+      before do
+        put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
+      end
+
+      context 'when the request params are valid' do
+        let(:user_params) { { email: 'new@email.com' } }
+       
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
         end
-        
+      
+        it 'returns the json data for the updated user' do
+          expect(json_body[:email]).to eq(user_params[:email])
+        end
       end
 
       context 'when the request params are invalid' do
-        let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
-
+        let(:user_params) { { email: 'invalid@' } }
         it 'returns status code 422' do
-            expect(response).to have_http_status(422)
+          expect(response).to have_http_status(422)
         end
-
-        it 'returns the json data for the errors' do
-          user_response = JSON.parse(response.body)
-          expect(user_response).to have_key('errors')          
+      
+        it 'returns erros for the updated user' do
+          expect(json_body).to have_key(:errors)
         end
         
+      end
+    end
+
+    describe 'DELETE /users/:id' do
+      before do
+        delete "/users/#{user_id}", params: {}, headers: headers
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'removes the user from database' do
+        expect(User.find_by(id: user.id)).to be_nil
       end
     end
 end
