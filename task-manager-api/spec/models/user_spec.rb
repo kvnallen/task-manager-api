6d397ff02d3f3de575c7e486911657b43a56c817
Kevin Allen <kevinallenlf@gmail.com>
2017-08-07 23:16:16 -0300

    add auth token to user

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -20,4 +20,34 @@ RSpec.describe User, type: :model do
 
   #   it { expect(user).not_to be_valid }
   # end
+
+  it{ is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'returns email, created_at and a token' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return('lolabc')
+
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
+    end
+  end
+
+  describe '#generate_authentication_token!' do
+    
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('lolabc')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq('lolabc')
+    end
+
+    it 'generates another auth token when the current auth token already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return( 'abc123tokenxyz','abc123tokenxyz','final-token')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+      
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
+
 end
